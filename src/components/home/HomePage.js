245de75b0ffc32de15e171eeb0712cd7f7009315
PrimeSoup <shakemaj96@gmail.com>
2021-08-29 20:38:56 +0200

    formatting of home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,15 +3,15 @@ import profile from "../../Profile_Photo.png";
 import Contact from "./Contact";
 
 const HomePage = () => (
-  <div className="flex flex-col justify-center items-center mx-5 space-y-5">
-    <h1 className="h1">Hi, I&apos;m Shakeeb</h1>
-    <div className="flex flex-row items-start m-32 space-x-4">
-      <img
-        className="object-scale-down h-96 w-full"
-        src={profile}
-        alt="Profile"
-      />
-      <p id="blurb" className="center">
+  <div className="flex flex-row items-center mt-10 mx-32 space-x-4">
+    <img
+      className="object-scale-down h-96 w-full"
+      src={profile}
+      alt="Profile"
+    />
+    <div className="flex flex-col justify-center items-center space-y-4">
+      <h1 className="h1">Hi, I&apos;m Shakeeb</h1>
+      <p className="text-center">
         I am a former software engineer and currently a masters student at
         Radboud University studying Neurophysics. I am interested in
         programming, brazilian jiu-jitsu, and self-studying modern physics.
@@ -20,8 +20,8 @@ const HomePage = () => (
         machine learning algorithms as I believe the brain has a lot to teach us
         about intelligence.
       </p>
+      <Contact />
     </div>
-    <Contact />
   </div>
 );
 
